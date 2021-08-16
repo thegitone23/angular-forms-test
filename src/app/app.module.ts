f9e8d661899284ca868e4e47ed2d98594c6e811b
Mayank Gautam <mayankgautam98@gmail.com>
2021-08-16 21:17:18 +0530

    generated user-data-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDataFormComponent } from './user-data-form/user-data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDataFormComponent
   ],
   imports: [
     BrowserModule,
